7000ef06127bed2bf9fda2e30d03f9b2c677e20c
Lay <karomahlailatul@gmail.com>
2024-03-17 09:30:28 +0700

    fix

--- dart-oop-challanges/bin/dart_oop_challanges.dart
+++ dart-oop-challanges/bin/dart_oop_challanges.dart
@@ -4,21 +4,30 @@
 //   print('Hello world: ${dart_oop_challanges.calculate()}!');
 // }
 
+import 'dart:io';
 
 class rumah {
     String? nama;
     String? alamat;
     int? jumlahKamar;
 
-    void rumahCheck(){
+    void display(){
         print("Rumah ${nama} berada di ${alamat} dengan jumlah kamar ${jumlahKamar}");
     }
 }
 
 void main(){
     rumah rumahCheck = rumah();
-    rumahCheck.nama = "laila";
-    rumahCheck.alamat = "jepara";
-    rumahCheck.jumlahKamar = 2;
-    rumahCheck.rumahCheck();
+    // rumahCheck.nama = "laila";
+    // rumahCheck.alamat = "jepara";
+    // rumahCheck.jumlahKamar = 2;
+
+    stdout.write("Masukkan nama rumah: ");
+    rumahCheck.nama = stdin.readLineSync();
+    stdout.write("Masukkan alamat rumah: ");
+    rumahCheck.alamat = stdin.readLineSync();
+    stdout.write("Masukkan jumlah kamar rumah: ");
+    rumahCheck.jumlahKamar = int.parse(stdin.readLineSync()!);
+
+    rumahCheck.display();
 }
