002c16fbf0295f152aa6793b836bf3e2ae3d2434
Lay <karomahlailatul@gmail.com>
2024-03-17 09:42:29 +0700

    lat1-1

---
+++ dart-oop-181240000851/bin/dart_oop_181240000851.dart
@@ -0,0 +1,5 @@
+import 'package:dart_oop_181240000851/dart_oop_181240000851.dart' as dart_oop_181240000851;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_oop_181240000851.calculate()}!');
+}
---
+++ dart-oop-181240000851/test/dart_oop_181240000851_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_oop_181240000851/dart_oop_181240000851.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
