73cb3350eddb979c339b1619d7faccee6392f1f1
Lay <karomahlailatul@gmail.com>
2024-03-17 09:56:18 +0700

    test-4

---
+++ dart-oop-181240000851/bin/lat1/4.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+class segitiga {
+  double? alas;
+  double? tinggi;
+  String? jenis;
+
+  segitiga(double alas, double tinggi, String jenis) {
+    this.alas = alas;
+    this.tinggi = tinggi;
+    this.jenis = jenis;
+  }
+  double luas() {
+    return 0.5 * alas! * tinggi!;
+  }
+
+  void display() {
+    print(
+        "Segitiga ${jenis} dengan alas ${alas} dan tinggi ${tinggi} memiliki luas ${luas()}");
+  }
+}
+
+void main() {
+  segitiga segitigaCheck = segitiga(5, 10, "siku-siku");
+  segitigaCheck.display();
+  stdout.write("Masukkan alas segitiga: ");
+  segitigaCheck.alas = double.parse(stdin.readLineSync()!);
+  stdout.write("Masukkan tinggi segitiga: ");
+  segitigaCheck.tinggi = double.parse(stdin.readLineSync()!);
+  segitigaCheck.display();
+}
+
